032091fd19ebcc6933612356fa639fbd2b5ad578
GYRAG <kinkladzegary@gmail.com>
2025-12-07 12:51:14 -0800

    Use dynamic PORT for cloud deployment

--- server/index.ts
+++ server/index.ts
@@ -102,7 +102,7 @@ io.on('connection', (socket: Socket) => {
     });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
     console.log(`Secure Uplink established on port ${PORT}`);
 });
